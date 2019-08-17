03c144c1bd813d7f51470b6dfe2005d1c032c15b
Yutyo <tanakinci2002@gmail.com>
2019-08-17 17:32:30 +0300

    first commit ^-^

---
+++ php/theme.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ *   * Name: Maker Birliği Network Theme 2
+ *   * Description: This is the third version of hub.mercode.org's theme". It is a fork of Bluebasic.
+ *   * Version: Theme Version 20181204<br/><br/>
+ *   * MinVersion: 2.3.1
+ *   * MaxVersion: 4.9.9
+ *   * Author: Tan Siret AKINCI (Yutyo)
+ */
+
+function bluebasic_init(&$a) {
+}
